ee8bf69431fbdd807c15b1fd422c864c2da0b05c
namtran02022003 <tranhuytunam02022003@gmail.com>
2022-09-05 09:42:53 +0700

    example4

---
+++ src/App1.js
@@ -0,0 +1,328 @@
+import './App1.css'
+
+function App () {
+    return (
+        <div className="app">
+        <div className="menu-25">
+          <div className="top-menu-25">
+            <img style={{borderRadius: '4px'}} src="https://www.w3schools.com/w3images/avatar_g2.jpg" width="45%" />
+            <div className="text">
+              <h2>PORTFOLIO</h2>
+              <p>Template by W3.CSS</p>
+            </div>
+          </div>
+          <div style={{paddingTop: 0}} className="list-menu-25 top-menu-25">
+            <a className="a-menu-25" href="#">
+              <i className="fa h fa-th-large fa-fw " />
+              FORTFOLIO
+            </a>
+            <a className="a-menu-25" href="#">
+              <i className="fa h fa-user fa-fw " />
+              ABOUT
+            </a>
+            <a className="a-menu-25 " href="#">
+              <i className="fa h fa-envelope fa-fw " />
+              CONTACT
+            </a>
+            <div className="icon-footer-menu-25">
+              <i className="fa fa-facebook-official w3-hover-opacity" />
+              <i className="fa fa-instagram w3-hover-opacity" />
+              <i className="fa fa-snapchat w3-hover-opacity" />
+              <i className="fa fa-pinterest-p w3-hover-opacity" />
+              <i className="fa fa-twitter w3-hover-opacity" />
+              <i className="fa fa-linkedin w3-hover-opacity" />
+            </div>
+          </div>
+        </div>
+        <div className="menu-75">
+          <div className="menu-75-content">
+            <h1 style={{margin: 0, fontSize: '36px'}}>My Portfolio</h1>
+            <div className="list-menu-75">
+              <p>fillter:
+                <a className="a-list-menu-75 n div-padding" href="#">ALL</a>
+                <a className="a-list-menu-75 div-padding" href="#">
+                  <i className="fa fa-diamond " />
+                  Design</a>
+                <a className="a-list-menu-75 div-padding" href="#">
+                  <i className="fa fa-photo " />
+                  Photos</a>
+                <a className="a-list-menu-75 div-padding" href="#">
+                  <i className="fa fa-map-pin " />
+                  Art</a>
+              </p>
+              <div className="div-rong" />
+            </div>
+            <div className="div-image-content">
+              <div className="div-flex-top ">
+                <div className="menu-image-small">
+                  <div className="image-content-small">
+                    <img className="opacity-hover" style={{width: '100%'}} src="https://www.w3schools.com/w3images/mountains.jpg" />
+                  </div>
+                  <div className="text-content-small">
+                    <p className="div-padding"><b>Lorem Ipsum</b> </p>
+                    <p className="div-padding">Praesent tincidunt sed tellus ut rutrum. Sed vitae justo
+                      condimentum,
+                      porta lectus vitae, ultricies congue gravida diam non fringilla.
+                    </p>
+                  </div>
+                </div>
+                <div className="menu-image-small">
+                  <div className="image-content-small">
+                    <img className="opacity-hover" style={{width: '100%'}} src="https://www.w3schools.com/w3images/lights.jpg" />
+                  </div>
+                  <div className="text-content-small">
+                    <p className="div-padding"><b>Lorem Ipsum</b> </p>
+                    <p className="div-padding">Praesent tincidunt sed tellus ut rutrum. Sed vitae justo
+                      condimentum,
+                      porta lectus vitae, ultricies congue gravida diam non fringilla.
+                    </p>
+                  </div>
+                </div>
+                <div className="menu-image-small">
+                  <div className="image-content-small">
+                    <img className="opacity-hover" style={{width: '100%'}} src="https://www.w3schools.com/w3images/nature.jpg" />
+                  </div>
+                  <div className="text-content-small">
+                    <p className="div-padding"><b>Lorem Ipsum</b> </p>
+                    <p className="div-padding">Praesent tincidunt sed tellus ut rutrum. Sed vitae justo
+                      condimentum,
+                      porta lectus vitae, ultricies congue gravida diam non fringilla.
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div style={{margin: '25px 0'}} className="div-flex-top">
+                <div className="menu-image-small">
+                  <div className="image-content-small">
+                    <img className="opacity-hover" style={{width: '100%'}} src="https://www.w3schools.com/w3images/mountains.jpg" />
+                  </div>
+                  <div className="text-content-small">
+                    <p className="div-padding"><b>Lorem Ipsum</b> </p>
+                    <p className="div-padding">Praesent tincidunt sed tellus ut rutrum. Sed vitae justo
+                      condimentum,
+                      porta lectus vitae, ultricies congue gravida diam non fringilla.
+                    </p>
+                  </div>
+                </div>
+                <div className="menu-image-small">
+                  <div className="image-content-small">
+                    <img className="opacity-hover" style={{width: '100%'}} src="https://www.w3schools.com/w3images/lights.jpg" />
+                  </div>
+                  <div className="text-content-small">
+                    <p className="div-padding"><b>Lorem Ipsum</b> </p>
+                    <p className="div-padding">Praesent tincidunt sed tellus ut rutrum. Sed vitae justo
+                      condimentum,
+                      porta lectus vitae, ultricies congue gravida diam non fringilla.
+                    </p>
+                  </div>
+                </div>
+                <div className="menu-image-small">
+                  <div className="image-content-small">
+                    <img className="opacity-hover" style={{width: '100%'}} src="https://www.w3schools.com/w3images/nature.jpg" />
+                  </div>
+                  <div className="text-content-small">
+                    <p className="div-padding"><b>Lorem Ipsum</b> </p>
+                    <p className="div-padding">Praesent tincidunt sed tellus ut rutrum. Sed vitae justo
+                      condimentum,
+                      porta lectus vitae, ultricies congue gravida diam non fringilla.
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className="list1234">
+                <a className="a-content-list  a-list-menu-75" href="#"> « </a>
+                <a className="a-content-list n a-list-menu-75" href="#"> 1 </a>
+                <a className="a-content-list a-list-menu-75" href="#"> 2 </a>
+                <a className="a-content-list a-list-menu-75" href="#"> 3 </a>
+                <a className="a-content-list a-list-menu-75" href="#"> 4 </a>
+                <a className="a-content-list a-list-menu-75" href="#"> » </a>
+              </div>
+            </div>
+          </div>
+          <div className="list-anh2">
+            <div className="image1">
+              <img src="https://www.w3schools.com/w3images/avatar_g.jpg" style={{width: '100%'}} />
+            </div>
+            <div className="image1">
+              <img src="https://www.w3schools.com/w3images/me2.jpg" style={{width: '100%'}} />
+            </div>
+          </div>
+          <div className="div-padding div-opacity">
+            <h2>ABOUT ME</h2>
+            <p>Just me, myself and I, exploring the universe of unknownment. I have a heart of love and an
+              interest of lorem ipsum and mauris neque quam blog. I want to share my world with you.
+              Praesent tincidunt sed tellus ut rutrum. Sed vitae justo condimentum, porta lectus vitae,
+              ultricies congue gravida diam non fringilla. Praesent tincidunt sed tellus ut rutrum. Sed
+              vitae justo condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla.
+            </p>
+            <hr style={{opacity: '0.3'}} />
+            <h2>Technical Skills</h2>
+            <p>Photography</p>
+            <div className="div-big">
+              <div className="div-small">95%</div>
+            </div>
+            <p>Web Design</p>
+            <div className="div-big">
+              <div style={{width: '85%'}} className="div-small">85%</div>
+            </div>
+            <p>Photoshop</p>
+            <div className="div-big">
+              <div style={{width: '80%'}} className="div-small">80%</div>
+            </div>
+            <button className="button-opacity">
+              <i style={{marginRight: '5px'}} className="fa fa-download " />Download resume
+            </button>
+            <hr style={{opacity: '0.3'}} />
+          </div>
+          <div className="div-padding">
+            <h2>How much I charge</h2>
+            <div className="div-flex-top">
+              <div style={{border: '1px solid #ddd'}} className="menu-image-small div-center div-nam ">
+                <h1 className="h1-big" style={{margin: 0}}>basic</h1>
+                <hr style={{marginTop: 0}} />
+                <p className="p-big">Web design</p>
+                <hr />
+                <p className="p-big">photography</p>
+                <hr />
+                <p className="p-big">1GB stogare</p>
+                <hr />
+                <p className="p-big">Mail suport</p>
+                <hr />
+                <p className="p-big">$ 10</p>
+                per month
+                <hr style={{marginBottom: 0}} />
+                <p style={{backgroundColor: '#f1f1f1'}} className="p-big">
+                  <button className="button3">sign up</button>
+                </p>
+              </div>
+              <div style={{border: '1px solid #ddd'}} className="menu-image-small div-center div-nam">
+                <h1 className="h1-big" style={{margin: 0, backgroundColor: '#009688'}}>basic</h1>
+                <hr style={{marginTop: 0}} />
+                <p className="p-big">Web design</p>
+                <hr />
+                <p className="p-big">photography</p>
+                <hr />
+                <p className="p-big">1GB stogare</p>
+                <hr />
+                <p className="p-big">Mail suport</p>
+                <hr />
+                <p className="p-big">$ 10</p>
+                per month
+                <hr style={{marginBottom: 0}} />
+                <p style={{backgroundColor: '#f1f1f1'}} className="p-big">
+                  <button className="button3">sign up</button>
+                </p>
+              </div>
+              <div style={{border: '1px solid #ddd'}} className="menu-image-small div-center div-nam">
+                <h1 className="h1-big" style={{margin: 0}}>basic</h1>
+                <hr style={{marginTop: 0}} />
+                <p className="p-big">Web design</p>
+                <hr />
+                <p className="p-big">photography</p>
+                <hr />
+                <p className="p-big">1GB stogare</p>
+                <hr />
+                <p className="p-big">Mail suport</p>
+                <hr />
+                <p className="p-big">$ 10</p>
+                per month
+                <hr style={{marginBottom: 0}} />
+                <p style={{backgroundColor: '#f1f1f1'}} className="p-big">
+                  <button className="button3">sign up</button>
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="div-mau">
+            <div style={{opacity: '0.75'}} className="div-padding">
+              <h2>Contact Me</h2>
+              <div className="div-flex-icon">
+                <div className="div-flex-icon-small n">
+                  <p style={{fontSize: '34px'}}><i className="fa  fa-envelope " />
+                  </p>
+                  <p>email@email.com</p>
+                </div>
+                <div style={{backgroundColor: '#009688'}} className="div-flex-icon-small ">
+                  <p style={{fontSize: '34px'}}><i className="fa  fa-map-marker " />
+                  </p>
+                  <p>Chicago, US</p>
+                </div>
+                <div className="div-flex-icon-small n">
+                  <p style={{fontSize: '34px'}}><i className="fa  fa-phone " />
+                  </p>
+                  <p>512312311</p>
+                </div>
+              </div>
+              <hr style={{marginTop: '30px', opacity: '0.5'}} />
+            </div>
+            <div className="div-padding">
+              <label>name</label>
+              <input type="text" className="input-form" />
+              <label>email</label>
+              <input type="text" className="input-form" />
+              <label>message</label>
+              <input type="text" className="input-form" />
+              <button className="button"><i className="fa fa-paper-plane " />
+                send message</button>
+            </div>
+            <div className="footer">
+              <div className="div-flex-footer">
+                <div className="menu-flex-footer">
+                  <h2>FOOTER</h2>
+                  <p>Praesent tincidunt sed tellus ut rutrum. Sed vitae justo condimentum,
+                    porta lectus vitae, ultricies congue gravida diam non fringilla.
+                  </p>
+                  <p>Powered by w3.css</p>
+                </div>
+                <div className="menu-flex-footer">
+                  <h2>BLOG POSTS</h2>
+                  <div className="menu-footer9">
+                    <div className="menu-footer-small">
+                      <img style={{width: '50px'}} src="https://www.w3schools.com/w3images/workshop.jpg" />
+                    </div>
+                    <div className="menu-footer-small">
+                      <h4 style={{margin: 0}}>Lorem</h4>
+                      <p style={{margin: 0}}>Sed mattis nunc</p>
+                    </div>
+                  </div>
+                  <hr style={{margin: 0}} />
+                  <div className="menu-footer9">
+                    <div className="menu-footer-small">
+                      <img style={{width: '50px'}} src="https://www.w3schools.com/w3images/gondol.jpg" />
+                    </div>
+                    <div className="menu-footer-small">
+                      <h4 style={{margin: 0}}>Ipsum</h4>
+                      <p style={{margin: 0}}>Praes tinci sed</p>
+                    </div>
+                  </div>
+                </div>
+                <div className="menu-flex-footer">
+                  <h2>POPULAR TAGS</h2>
+                  <p>
+                    <span className="p-footer n">Travel</span>
+                    <span className="p-footer"> New York</span>
+                    <span className="p-footer">London</span>
+                    <span className="p-footer">IKEA</span>
+                    <span className="p-footer">NORWAY</span>
+                    <span className="p-footer">DIY</span>
+                    <span className="p-footer">Ideas</span>
+                    <span className="p-footer">Baby</span>
+                    <span className="p-footer">Family</span>
+                    <span className="p-footer">News</span>
+                    <span className="p-footer">Clothing</span>
+                    <span className="p-footer">Shopping</span>
+                    <span className="p-footer">Sports</span>
+                    <span className="p-footer">Games</span>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="footer-top">
+            <p className="footer-top-p">Powered by w3.css</p>
+          </div>
+        </div>
+      </div>
+    )
+}
+export default App;
